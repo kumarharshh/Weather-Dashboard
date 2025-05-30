43ceb022fd8872c3b27cc105e031d9c3a48e27f8
Harsh <bunnynick10@gmail.com>
2025-05-30 20:56:39 +0530

    feat: refactor WeatherDetails and WeatherForecast components to use @tanstack/react-query for data fetching

--- src/components/UI/WeatherForecast.jsx
+++ src/components/UI/WeatherForecast.jsx
@@ -1,38 +1,30 @@
 import styles from './WeatherForecast.module.css';
-import { useEffect, useState } from "react";
 import { useCity } from "../../lib/hooks/useCity";
 import fetchWeatherForecast from "../../api/weatherForecastAPI";
 import WeatherInfo from "./WeatherInfo";
+import { useQuery } from '@tanstack/react-query';
 
 export default function WeatherForecast() {
     const { city } = useCity();
-    const [forecastData, setForecastData] = useState(null);
 
-    useEffect(() => {
-        let intervalId;
-        const getForecastData = async () => {
-            try {
-                setForecastData(null);
-                const data = await fetchWeatherForecast(city);
-                setForecastData(data);
-            } catch {
-                setForecastData(null);
-                return; 
-            }
-        };
-        getForecastData();
+    const { data, isLoading } = useQuery({
+        queryKey: ['forecast', city],
+        queryFn: () => fetchWeatherForecast(city),
+        refetchInterval: 30000, 
+        staleTime: 1800000, 
+        enabled: !!city,
+    });
 
-        intervalId = setInterval(getForecastData, 180000); // 3 minutes
-        return () => clearInterval(intervalId);
-    }, [city]);
-
-    const dailyForecasts = forecastData?.list.filter(item =>
+    const dailyForecasts = data?.list.filter(item =>
         item.dt_txt.includes("12:00:00")
     );
 
     return (
         <>
-            {forecastData && (
+            {isLoading && (
+                <div className={styles.loading}>Loading forecast...</div>
+            )}
+            {data && (
                 <div className={styles.weatherForecast}>
                     <div className={styles.forecastHeading}>
                         Weather Forecast for {city}
